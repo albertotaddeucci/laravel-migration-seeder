bb62c16d3609d6054e5d69d814a2d197becee798
alberto <alberto.taddeucci@gmail.com>
2024-04-10 17:15:20 +0200

    messa in pagina dei treni

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,53 @@
 
 @section('content')
 
-{{-- content here --}}
+<table class="table">
+    <thead>
+      <tr>
+        <th>Partenza da </th>
+        <th>Arrivo a</th>
+        <th>Orario di partenza</th>
+        <th>Orario di arrivo</th>
+        <th>Treno Nº</th>
+        <th>In orario</th>
+        <th>Cancellato</th>
 
+        
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($trains as $train)
+            <tr>
+                
+                <td>
+                    {{$train->departure_station}}
+                </td>
+                <td>
+                    {{$train->arrival_station}}
+                </td>
+                <td>
+                    {{$train->departure_time}}
+                </td>
+                <td>
+                    {{$train->arrival_time}}
+                </td>
+                
+                <td>
+                    {{$train->train_code}}
+                </td>
+                <td>
+                    {{$train->on_schedule}}
+                </td>
+                <td>
+                    {{$train->is_cancelled}}
+                </td>
+                
+                
+                
+            </tr>
+        @endforeach
+    </tbody>
+  </table>
+
+        
 @endsection
